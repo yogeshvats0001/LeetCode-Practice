592acd71a5aa17bcc12a6b6ca1eb3f3c96cf3768
Yogesh Vats <yogisharma1062@gmail.com>
2022-07-17 23:41:53 +0530

    Concatenation of Array

---
+++ concatenateArray.java
@@ -0,0 +1,26 @@
+public class concatenateArray{
+    public static int[] solution(int[] nums){
+        int len = nums.length;
+        int[] ans = new int[2*len];
+        // ans = nums; wrong step as it will reassign the address of nums array to new ans array.
+        int pointer = 0;
+        int turn = 1;
+        while(turn < 3){
+            for(int i =0; i<nums.length;i++){
+                ans[pointer++] = nums[i];
+            }
+            turn++;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        //inputs (Hard Coded)
+        int[] nums = {1,2,1,3};
+        int[] res = solution(nums);
+        System.out.println("res length : "+res.length);
+        for(int i=0;i<res.length;i++){
+            System.out.print(res[i]  + " ");
+        }
+    }
+}
