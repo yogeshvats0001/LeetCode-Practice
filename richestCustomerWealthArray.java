403d2d77ffdbef88424f706755ec2266146680a7
Yogesh Vats <yogisharma1062@gmail.com>
2022-07-22 05:10:50 +0530

    1672. Richest Customer Wealth

--- richestCustomerWealthArray.java
+++ richestCustomerWealthArray.java
@@ -1,4 +1,5 @@
 // 1672. Richest Customer Wealth
+
 // You are given an m x n integer grid accounts where accounts[i][j] is the amount of money the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank. Return the wealth that the richest customer has.
 
 // Input:
