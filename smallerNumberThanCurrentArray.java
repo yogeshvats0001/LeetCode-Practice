91e04d14df9559b7adca19cf3ac688454f81f111
Yogesh Vats <yogisharma1062@gmail.com>
2022-07-22 05:13:19 +0530

    1365. How Many Numbers Are Smaller Than the Current Number

--- smallerNumberThanCurrentArray.java
+++ smallerNumberThanCurrentArray.java
@@ -1,5 +1,4 @@
 // 1365. How Many Numbers Are Smaller Than the Current Number
-
 // Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it. That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
 
 // Input: nums = [8,1,2,2,3]
