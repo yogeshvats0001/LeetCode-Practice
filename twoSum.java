26915806ec415b9046579ceb675a699741bea5a1
Yogesh Vats <yogisharma1062@gmail.com>
2022-07-28 21:34:17 +0530

    1. Two Sum

--- twoSum.java
+++ twoSum.java
@@ -9,8 +9,9 @@ Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
  * Input: nums = [3,3], target = 6
 Output: [0,1]
  */
-
+import java.util.HashMap;
 public class twoSum {
+    //Method1. Simple for loops with n^2 complexity.
     public static int[] sumPair(int[] nums, int target){
         for(int i=0; i<nums.length-1; i++){
             int second = target - nums[i];
@@ -24,10 +25,24 @@ public class twoSum {
         return new int[]{};
     }
 
+    //Method2. Lets try some optimisation
+    public static int[] sumPairWithHashMap(int[] nums, int target){
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            int secondTerm = target - nums[i];
+            if(map.containsKey(secondTerm)){
+                return new int[]{map.get(secondTerm), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+
     public static void main(String[] args) {
         int[] nums = {2,7,11,15};
         int target = 9;
-        int[] resultArr = sumPair(nums, target);
+        // int[] resultArr = sumPair(nums, target);
+        int[] resultArr = sumPairWithHashMap(nums, target);
         for(int element : resultArr ){
             System.out.print(element + " ");
         }
