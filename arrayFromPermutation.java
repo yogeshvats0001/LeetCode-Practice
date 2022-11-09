804d606154caf297782f0058829c857f6171e585
Yogesh Vats <yogisharma1062@gmail.com>
2022-11-09 07:20:05 +0530

    1920. Build Array from Permutation

--- arrayFromPermutation.java
+++ arrayFromPermutation.java
@@ -7,6 +7,7 @@ Input: nums = [0,2,1,5,3,4]
 Output: [0,1,2,4,5,3]*/
 
 public class arrayFromPermutation {
+    //Method1. Iterative Way
     public static int[] permutation(int[] nums){
         int[] ans = new int[nums.length];
         for(int i=0; i<nums.length; i++){
@@ -15,11 +16,29 @@ public class arrayFromPermutation {
         return ans;
     }
 
+    //Method2. Recursive Way (Since answer depends upon the smaller and smaller part of the array)
+    public static int[] permutationRecursion(int[] nums, int[] newArr, int index){
+        //BC
+        if(index == nums.length) return newArr;
+        //MC => f(n) = g(g(n))
+        newArr[index] = nums[nums[index]];
+        //RC
+        return permutationRecursion(nums, newArr, index+1);
+    }
+
     public static void main(String[] args) {
         int[] nums = {0,2,1,5,3,4};
-        int[] result = permutation(nums);
-        for(int i : result){
-            System.out.print(i + " ");
+        //Method1. Iteration
+        // int[] result = permutation(nums);
+        // for(int i : result){
+        //     System.out.print(i + " ");
+        // }
+
+        //Method2. Recursion
+        int[] newArr = new int[nums.length];
+        nums = permutationRecursion(nums, newArr, 0);
+        for(int element : nums){
+            System.out.print(element + " ");
         }
     }
 }
